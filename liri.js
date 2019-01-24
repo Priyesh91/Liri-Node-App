841cd05178eb18c8edcf53764fc0837bab33f17c
Priyesh <91priyesh@gmail.com>
2019-01-24 18:36:41 -0500

    spotify call working, bands in town laid out but not retreiving proper response

--- liri.js
+++ liri.js
@@ -1,24 +1,58 @@
 require("dotenv").config();
 
-let keys = require("./keys.js");
+const keys = require("./keys.js");
 
-console.log(process.env.SPOTIFY_ID);
-console.log(process.env.SPOTIFY_SECRET);
-let Spotify = require("node-spotify-api");
+const axios = require("axios");
 
-let spotify = new Spotify(keys.spotify);
+// console.log(process.env.SPOTIFY_ID);
+// console.log(process.env.SPOTIFY_SECRET);
 
-console.log(spotify);
+const Spotify = require("node-spotify-api");
+
+const spotify = new Spotify(keys.spotify);
+
+// console.log(spotify);
 
 const inputCommand = process.argv[2];
+const inputCommand2 = process.argv[3];
 
-function concertThis(){
+function spotifySong() {
+
+  spotify
+    .search({
+      type: 'track',
+      query: inputCommand2,
+      limit: 1
+    })
+    .then(function (response) {
+      // console.log(response);
+      let song = response.tracks.items[0];
+      // console.log(song);
+      console.log(`----------
+    Title: ${song.name}
+    Artist: ${song.artists[0].name}
+    Album: ${song.album.name}
+    Preview Link: ${song.preview_url}
+    ----------`);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+  // console.log("SpotifySongActivated");
+};
+
+
+function concertThis() {
+  let queryurl = (`https://rest.bandsintown.com/artists/${inputCommand2}/events?app_id=codingbootcamp`);
+  // console.log(queryurl);
+  axios.get(queryurl).then(
+    function (response) {
+      console.log(response);
+    }
+  )
   console.log("working 1");
 };
 
-function spotifySong() {
-  console.log("working 2");
-};
 
 function movieThis() {
   console.log("working 3");
@@ -31,15 +65,15 @@ function doWhatItSays() {
 
 switch (inputCommand) {
   case "concert-this":
-  return concertThis();
+    return concertThis();
 
   case "spotify-this-song":
-  return spotifySong();
+    return spotifySong();
 
   case "movie-this":
-  return movieThis();
+    return movieThis();
 
   case "do-what-it-says":
-  return doWhatItSays();
+    return doWhatItSays();
 
 };
