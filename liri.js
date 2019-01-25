992154a1f5c2d2f1d602ed9d48e4c2647f9a87ea
Priyesh <91priyesh@gmail.com>
2019-01-24 20:49:49 -0500

    concert-this function working with bands in town

--- liri.js
+++ liri.js
@@ -13,11 +13,13 @@ const spotify = new Spotify(keys.spotify);
 
 // console.log(spotify);
 
+//user input of predetermined command
 const inputCommand = process.argv[2];
+// user input
 const inputCommand2 = process.argv[3];
 
+//function for spotify
 function spotifySong() {
-
   spotify
     .search({
       type: 'track',
@@ -41,39 +43,49 @@ function spotifySong() {
   // console.log("SpotifySongActivated");
 };
 
-
+//function for bands in town
 function concertThis() {
   let queryurl = (`https://rest.bandsintown.com/artists/${inputCommand2}/events?app_id=codingbootcamp`);
   // console.log(queryurl);
   axios.get(queryurl).then(
     function (response) {
-      console.log(response);
-    }
-  )
-  console.log("working 1");
-};
+      // console.log(JSON.stringify(response.data, null, 2));
+      // console.log(inputCommand2);
+      // console.log(JSON.stringify(response.data[0].venue.name));
+      // console.log(JSON.stringify(response.data[0].venue.country));
+      // console.log(JSON.stringify(response.data[0].venue.city));
+      // console.log(JSON.stringify(response.data[0].datetime));
+      (response.data.length === 0) ?
+       console.log("No Avaible data"):
+       console.log(response.data[0].venue.name);
+      console.log(response.data[0].venue.country);
+      console.log(response.data[0].venue.city);
+      console.log(response.data[0].datetime); 
+    });
+    
+  };
 
 
-function movieThis() {
-  console.log("working 3");
-};
+  function movieThis() {
+    console.log("working 3");
+  };
 
-function doWhatItSays() {
-  console.log("working 4");
-};
+  function doWhatItSays() {
+    console.log("working 4");
+  };
 
 
-switch (inputCommand) {
-  case "concert-this":
-    return concertThis();
+  switch (inputCommand) {
+    case "concert-this":
+      return concertThis();
 
-  case "spotify-this-song":
-    return spotifySong();
+    case "spotify-this-song":
+      return spotifySong();
 
-  case "movie-this":
-    return movieThis();
+    case "movie-this":
+      return movieThis();
 
-  case "do-what-it-says":
-    return doWhatItSays();
+    case "do-what-it-says":
+      return doWhatItSays();
 
-};
+  };
